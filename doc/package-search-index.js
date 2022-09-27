99ee13ac2f5597fdb901f2db349d31cd654ee838
Matthias Rudolph <1193244+mtt1@users.noreply.github.com>
2022-09-27 20:18:49 +0200

    Doc upload

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"de.stocker.common"},{"l":"de.stocker.controller"},{"l":"de.stocker.json"},{"l":"de.stocker.model"},{"l":"de.stocker.model.dataWrappers"},{"l":"de.stocker.network"},{"l":"de.stocker.persistence"},{"l":"de.stocker.preferences"},{"l":"de.stocker.view"}]
